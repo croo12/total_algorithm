b93c1ebafc1185a940629a11e0c1d9635504d51f
croo12 <98068968+croo12@users.noreply.github.com>
2023-05-24 14:40:15 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 12 ms, Memory: 17056 KB -BaekjoonHub

--- 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.rs
+++ 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.rs
@@ -1,57 +1,27 @@
-use std::{io, collections::{HashSet, VecDeque}};
+use std::io;
 
 fn main() {
     let input = input();
 
-    let n_k : Vec<i32> = input.split_whitespace().map(|x| x.parse().unwrap()).collect();
-    let n = n_k.get(0).unwrap();
-    let k = n_k.get(1).unwrap();
+    let mut tokens = input.split_ascii_whitespace().flat_map(str::parse::<i32>);
+    let n = tokens.next().unwrap() as usize;
+    let k = tokens.next().unwrap();
 
-    // N <= K 무조건 가능
-    if n <= k {
-        println!("minigimbob");
-    }
-    // N > K
-    else {
-        if bfs(n, k) {
-            println!("minigimbob");
-        } else {
-            println!("water");
-        }
-    }
-}
+    let mut memo = vec![i32::MAX; n + 1];
+    memo[0] = 0;
 
-fn bfs(n :&i32, k :&i32) -> bool {
-
-    let mut q = std::collections::VecDeque::<i32>::new(); 
-    let mut vis = HashSet::<i32>::new();
-
-    q.push_back(0);
-    vis.insert(0);
-
-    let mut cnt = 0;
-
-    while !q.is_empty() && cnt <= *k {
-        let count = q.len();
-        cnt += 1;
-
-        for _ in 0..count {
-            let now = q.pop_front().unwrap();
-
-            if now == * n { return true; }
-
-            check(now + 1, &mut vis, &mut q);
-            check(now + (now >> 1), &mut vis, &mut q);
+    for i in 0..n {
+        memo[i + 1] = memo[i + 1].min(memo[i] + 1);
+        let value = i + (i >> 1);
+        if value <= n {
+            memo[value] = memo[value].min(memo[i] + 1);
         }
     }
 
-    false
-}
-
-fn check(next : i32, vis: &mut HashSet<i32>, q: &mut VecDeque<i32>) {
-    if !vis.contains(&next) {
-        vis.insert(next);
-        q.push_back(next);
+    if memo[n] > k {
+        println!("water");
+    } else {
+        println!("minigimbob")
     }
 }
 
