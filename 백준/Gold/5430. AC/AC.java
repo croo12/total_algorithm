b4c096dba3357be4feb692a12f53d1262c3083a6
croo12 <98068968+croo12@users.noreply.github.com>
2023-06-27 16:19:28 +0900

    [Gold V] Title: AC, Time: 544 ms, Memory: 33532 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,62 +1,106 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		while (T-- > 0) {
-			String p = br.readLine();
-			int n = Integer.parseInt(br.readLine());
-			String arr = br.readLine();
-			ArrayDeque<Integer> dq = new ArrayDeque<>();
-			boolean dir = true; // true : 정방향, false : 역방향
-			boolean flag = true;
-			// 문자열 분리 후 덱에 넣어준다
-			StringTokenizer st = new StringTokenizer(arr, "[],");
-			for (int i = 0; i < n; i++) {
-				dq.offer(Integer.parseInt(st.nextToken()));
-			}
-			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < p.length(); i++) {
-				char ch = p.charAt(i);
-				if (ch == 'R') {
-					dir = !dir;
-				} else {
-					if (dq.isEmpty()) {
-						sb.append("error");
-						flag = false;
-						break;
-					}
-					if (dir) {
-						dq.pollFirst();
-					} else {
-						dq.pollLast();
-					}
-				}
-			}
-			if (flag) {
-				sb.append("[");
-				if (dq.size() > 0) {
-					if (dir) {
-						while (!dq.isEmpty()) {
-							sb.append(dq.getFirst()).append(",");
-							dq.pollFirst();
-						}
-					} else {
-						while (!dq.isEmpty()) {
-							sb.append(dq.getLast()).append(",");
-							dq.pollLast();
-						}
-					}
-					sb.delete(sb.length() - 1, sb.length());
-				}
-				sb.append("]");
-				System.out.println(sb);
-			}else {
-				System.out.println(sb);
-			}
-		}
-	}
+    int testCase;
+    String[] orders;
+    int[] numberArrayLengths;
+    int[][] numberArrays;
+
+    private void solution() {
+        input();
+
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < testCase; i++) {
+            sb.append(doOrder(i)).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+
+    private String doOrder (int idx) {
+        char[] orderList = orders[idx].toCharArray();
+        int startLength = 0;
+        int endLength = numberArrayLengths[idx] - 1;
+
+        boolean isReverse = false;
+
+        for ( var order : orderList) {
+            switch ( order ) {
+                case 'R' :
+                    isReverse = !isReverse;
+                    break;
+                case 'D' :
+                    if ( startLength > endLength ) return "error";
+
+                    if ( !isReverse ) {
+                        startLength++;
+                    } else {
+                        endLength--;
+                    }
+            }
+        }
+
+        var sb = new StringBuilder();
+        sb.append('[');
+
+        if ( !isReverse ) {
+            for ( int i = startLength; i <= endLength; i++) {
+                sb.append(numberArrays[idx][i]).append(',');
+            }
+        } else {
+            for ( int i = endLength; i >= startLength; i--) {
+                sb.append(numberArrays[idx][i]).append(',');
+            }
+        }
+
+        if ( startLength <= endLength ) {
+            sb.setLength(sb.length() - 1);
+        }
+
+        sb.append(']');
+
+        return sb.toString();
+    }
+
+    void input() {
+        try {
+            var br = new BufferedReader(new InputStreamReader(System.in));
+            testCase = Integer.parseInt(br.readLine());
+
+            orders = new String[testCase];
+            numberArrayLengths = new int[testCase];
+            numberArrays = new int[testCase][];
+
+            for (int i = 0; i < testCase; i++ ) {
+                orders[i] = br.readLine();
+
+                int arrayLength = Integer.parseInt(br.readLine());
+                numberArrayLengths[i] = arrayLength;
+                numberArrays[i] = new int[arrayLength];
+
+                for ( int j = 0; j < arrayLength; j++) {
+                    int sum = 0;
+                    int now;
+
+                    while ( (now = br.read()) != ',' && now != ']' ) {
+                        if ( now >= '0' && now <= '9') {
+                            sum = sum*10 + now - '0';
+                        }
+                    }
+
+                    numberArrays[i][j] = sum;
+                }
+                br.readLine();
+            }
+        } catch( Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().solution();
+    }
 }
