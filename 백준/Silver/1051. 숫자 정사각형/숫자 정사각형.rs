f3e9a650999df2312ccf4c33094615cb4e3ccab6
croo12 <98068968+croo12@users.noreply.github.com>
2023-07-04 17:38:45 +0900

    [Silver III] Title: 숫자 정사각형, Time: 8 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.rs
@@ -0,0 +1,68 @@
+use std::io::stdin;
+
+fn main() {
+    let n_m : Vec<usize> = parse_vec(read_line());
+
+    let mut map_vector : Vec<Vec<char>> = Vec::new();
+
+    for idx in 0..n_m[0] {
+        let input = read_line();
+        let mut tmp = input.trim_end().chars();
+
+        map_vector.push(Vec::<char>::new());
+        loop {
+            let c = tmp.next();
+
+            if c == None {
+                break;
+            }
+
+            map_vector[idx].push(c.unwrap());
+        }
+    }
+
+    let max = n_m[0].min(n_m[1]);
+    let mut answer = 0;
+    for length in (1..max+1).rev() {
+        if check_square(length, &mut map_vector, n_m[0], n_m[1]) {
+            answer = length * length;
+            break;
+        }
+    }
+
+    println!("{}", answer);
+}
+
+fn check_square(length : usize, map_vector : &mut Vec<Vec<char>>, row : usize, col : usize ) -> bool {
+
+    for r in 0..(row-length+1) {
+        for c in 0..(col-length+1) {
+            if  map_vector[r][c] == map_vector[r + length - 1][c] && 
+                map_vector[r][c] == map_vector[r][c + length - 1] && 
+                map_vector[r][c] == map_vector[r + length - 1][c + length - 1] {
+
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+fn parse_vec<T : std::str::FromStr> (line : String ) -> Vec<T> 
+    where <T as std::str::FromStr>::Err: std::fmt::Debug  
+{ 
+    
+    let list: Vec<T> = line.split_whitespace()
+        .map(|w| w.parse::<T>().unwrap())
+        .collect(); 
+    
+    list
+}
+
+fn read_line() -> String {
+    let mut input = String::new();
+
+    stdin().read_line(&mut input).expect("can't read");
+    input
+}
