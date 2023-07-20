1f0045b78d5ed081919c9ad2a1ac2043b1c8922b
croo12 <98068968+croo12@users.noreply.github.com>
2023-07-20 17:07:22 +0900

    [Gold IV] Title: 데이터 체커, Time: 520 ms, Memory: 97636 KB -BaekjoonHub

--- 백준/Gold/22942. 데이터 체커/데이터 체커.java
+++ 백준/Gold/22942. 데이터 체커/데이터 체커.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Stream;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
