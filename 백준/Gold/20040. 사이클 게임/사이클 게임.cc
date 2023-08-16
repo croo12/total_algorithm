6747a4b4d2ea67a2f7cea59301e8fb4006411959
croo12 <98068968+croo12@users.noreply.github.com>
2023-08-16 16:57:51 +0900

    [Gold IV] Title: 사이클 게임, Time: 420 ms, Memory: 9704 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.cc
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -1,22 +1,23 @@
 #include <iostream>
 #include <vector>
 
-int find(int x, std::vector<int>& p) {
-	if (p[x] != x)
-		p[x] = find(p[x], p);
+int p[500'000];
 
-	return p[x];
+int find(int x) {
+	return p[x] == x ? x : p[x] = find(p[x]);
 }
 
-void union_numbers(int x, int y, std::vector<int>& p) {
-	p[x] = y;
+bool union_numbers(int x, int y) {
+	int xp = find(x);
+	int yp = find(y);
+	p[xp] = yp;
+	return xp != yp;
 }
 
 int main() {
 	int n, m;
 	std::cin >> n >> m;
 
-	std::vector<int> p(n, 0);
 	for (size_t i = 0; i < n; i++)
 	{
 		p[i] = i;
@@ -29,15 +30,10 @@ int main() {
 		int first, second;
 		std::cin >> first >> second;
 
-		first = find(first, p);
-		second = find(second, p);
-
-		if (first == second) {
+		if (!union_numbers(first, second)) {
 			std::cout << cnt << '\n';
 			return 0;
 		}
-
-		union_numbers(first, second, p);
 	}
 
 	std::cout << 0 << '\n';
