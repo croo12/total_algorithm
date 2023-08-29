5b59cbbfc061d8dde757d112352d808af4c5c03f
croo12 <98068968+croo12@users.noreply.github.com>
2023-08-29 14:31:21 +0900

    [Gold IV] Title: 오렌지 출하, Time: 0 ms, Memory: 2256 KB -BaekjoonHub

--- 백준/Gold/11985. 오렌지 출하/오렌지 출하.cc
+++ 백준/Gold/11985. 오렌지 출하/오렌지 출하.cc
@@ -9,8 +9,6 @@ int n, m, k;
 int A[MAX];
 ll cache[MAX];
 
-ll divide(int idx);
-
 int main() {
 	cin >> n >> m >> k;
 
@@ -22,27 +20,23 @@ int main() {
 	fill(cache, cache + n, -1);
 	cache[n] = 0;
 
-	ll answer = divide(0);
-	cout << answer << '\n';
-}
-
-ll divide(int idx) {
-	if (cache[idx] != -1) return cache[idx];
-
-	ll v = 9223372036854775807LL;
-	ll biggest = -1;
-	ll smallest = 1'000'000'001;
-
-	for (int i = idx; i < n && i < idx + m; ++i)
+	for (int i = n-1; i >= 0; --i)
 	{
-		int now = A[i];
-		if (biggest < now) biggest = now;
-		if (smallest > now) smallest = now;
+		ll v = 9223372036854775807LL;
+		ll biggest = -1;
+		ll smallest = 1'000'000'001;
 
-		ll calc = divide(i + 1) + k + (i - idx + 1) * (biggest - smallest);
-		if (calc < v) v = calc;
+		for (int j = i; j < n && j < i + m; j++) {
+			int now = A[j];
+			if (biggest < now) biggest = now;
+			if (smallest > now) smallest = now;
+
+			ll calc = cache[j + 1] + k + (j - i + 1) * (biggest - smallest);
+			if (calc < v) v = calc;
+		}
+
+		cache[i] = v;
 	}
 
-	cache[idx] = v;
-	return cache[idx];
+	cout << cache[0] << '\n';
 }
