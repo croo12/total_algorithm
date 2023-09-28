9b1b6952f72d78250603bbb341ad6ed85eb7b07d
croo12 <98068968+croo12@users.noreply.github.com>
2023-09-28 19:31:57 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    
+    while (true)
+    {
+        cin >> n >> m;
+        
+        if (n == 0 && m == 0) return 0;
+    
+        if (n > m)
+        {
+            cout << "Yes\n";
+        }
+        else 
+        {
+            cout << "No\n";
+        }
+    
+    }
+        
+} 
